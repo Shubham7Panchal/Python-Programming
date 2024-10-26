ed926aae451c2ac572e8af38e158b1e6824a7232
Shubham7Panchal <shubhampanchal2112@gmail.com>
2024-10-26 16:37:00 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+import pyjokes
+print("printing jokes....")
+# below line print a joke
+"""
+similarly here triple duble-quotes or double single-quotes are used for comment purpose
+"""
+joke = pyjokes.get_joke()
+print(joke)
---
+++ ps_problem1.py
@@ -0,0 +1,5 @@
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I am Shubham who is programming python right now.")
+engine.say("chai bolo chai.")
+engine.runAndWait()
